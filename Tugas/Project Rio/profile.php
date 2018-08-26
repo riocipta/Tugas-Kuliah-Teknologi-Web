48a757c4ade4554cf5dd80c47c3da1c115d986ac
Rio Cipta Nurandi <40756894+riocipta@users.noreply.github.com>
2018-08-26 14:40:53 +0700

    Add files via upload

---
+++ Tugas/Project Rio/profile.php
@@ -0,0 +1,39 @@
+<head>
+	<meta charset="utf-8">
+	<!-- pemanggilan file css -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta name="viewport" content="width=device-widget, initial-scale=1">
+		<title>kulgram</title>
+</head>
+	<?php
+ require('header.php');
+ ?>
+		<h1>My Profile</h1>
+	<div class="pro" >
+			<img src="image/satu.jpg" width=" 150px">
+			<ul type="circle">
+				<li>Nama	: Rio Cipta Nurandi</li>
+				<li>Nim		: 311610241</li>
+				<li>Kelas	: TI.16.B2</li>
+			</ul>
+	</div>
+	<div class="data">
+		<ul type="circle">
+			<h2>Biodata Lengkap</h2>
+			<li>Nama			:Rio Cipta Nurandi</li>
+			<li>Jenis Kelamin	:Laki - Laki</li>
+			<li>Ttl 			:Cilacap, 30 Maret 1998</li>
+			<li>Alamat			:Cikarang</li>
+			<li>Agama 			:Katholik</li>
+			<li>Kewarganegaraan :Indonesia</li>
+			<li>No.hp 			:081398315943</li>
+			<li>E-mail 			:riocipta@mhs.pelitabangsa.ac.id</li>
+		</ul>
+	</div>
+ <?php
+include_once 'footer.php';
+ ?>
+
+
+</body>
+</html>
